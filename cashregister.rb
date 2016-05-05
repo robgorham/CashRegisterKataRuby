4ac415ec9da0e54bee804d4351ff62ccfa5f1de9
Robert Gorham <robsteeliano@gmail.com>
2016-05-05 13:26:02 -0400

    Initial Commit

---
+++ cashregister.rb
@@ -0,0 +1,25 @@
+class Denom
+
+	include Comparable
+
+	@singular
+	@plural
+	@value
+
+	attr_reader :singular
+	attr_reader :plural
+	attr_reader :value
+
+	def initialize(singular, plural, value)
+		@singular = singular
+		@plural = plural
+		@value = value
+	end
+
+	def <=>(other)
+		@value <=> other.value
+	end
+end
+
+
+
